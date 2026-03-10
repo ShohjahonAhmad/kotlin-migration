0c22a588b9480858b3c99ace17810a9738ecc1f1
ShohjahonAhmad <ahmshohjahon@gmail.com>
2026-03-10 11:44:15 +0500

    cleaned up from unnecessary links

--- app/root.tsx
+++ app/root.tsx
@@ -13,20 +13,6 @@ import "./app.css";
 import Footer from "./components/footer/index";
 import Header from "./components/header";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "icon", href: "/favicon.ico" },
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
